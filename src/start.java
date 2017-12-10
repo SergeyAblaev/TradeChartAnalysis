9b8f56fd52cb53cd6d70b022d2e7ee347237961c
SergeyAblaev <ablaev.sergey@gmail.com>
2017-12-10 16:03:53 +0500

    try2

--- src/start.java
+++ src/start.java
@@ -1,6 +1,6 @@
 public class start {
     public static void main(String[] args) {
         System.out.println("test123");
-        System.out.println("");
+        System.out.println("456");
     }
 }
